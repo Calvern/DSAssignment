57d3fd151fa03d67b5e1dd9f357035ef897b6ace
yanhotan <tanyanho123@gmail.com>
2023-05-18 15:52:43 +0800

    update

--- TesterClass.java
+++ TesterClass.java
@@ -25,23 +25,17 @@ public class TesterClass {
         System.out.println("Given the text to encrypt and decrypt.");
         System.out.println("Origin Text: Advise Cao Cao to use The Chain Strategem, which is to chain his battleships with strong iron chains.");
         System.out.println("Encrypted Text: ^hkcpzl$^jhv$^jhv$av$bzl$^aol$^johpu$^zayhalnlt,$(ojpod)$pz$av$johpu$opz$(zwpozlsaahi)$dpao$zayvun$pyvu$johpuz.");
-//        System.out.print("Enter 'E/D' to encrypt or decrypt the text: ");
-//        String s = scanner.next();
-//        if (s.equals("E")) {
         intermediateText = Encryption.applySpecialSyntaxOperations(originTxt);
         encryptedText = Encryption.encrypt(intermediateText, shift);
-//        System.out.println("Encrypted Text: " + encryptedText + "\n");
 
-//        } else if (s.equals("D")) {
         decryptedIntermediateText = Decryption.SpecialSyntaxOperations(cipherText);
         decryptedText = Decryption.decrypt(decryptedIntermediateText, shift);
         System.out.println("Decrypted Text: " + decryptedText);
-//        }
 
         //AES encryption
         String AESEncryption = "";
         System.out.println("Starting from now, please enter 'y' for yes and 'n' for no when answering the questions.");
-        System.out.print("Do you want to use AES encryption? [y/n]: ");//1
+        System.out.print("Do you want to use AES encryption? [y/n]: ");
         String useAES = scanner.next();
         boolean AES = false;
         if (useAES.equals("y")) {
@@ -50,13 +44,13 @@ public class TesterClass {
             System.out.println("Encrypted Text (AES): " + AESEncryption);
         }
 
-        System.out.print("Do you want to use Caesar Cipher for further encryption - &num{}? [y/n]:");//2
+        //Applying Caesar Cipher - &num{}
+        System.out.print("Do you want to use Caesar Cipher for further encryption - &num{}? [y/n]:");
         String useCC = scanner.next();
         while (useCC.equals("y")) {
             System.out.print("Enter the start index and end index: ");
             int startIndex = scanner.nextInt();
             int endIndex = scanner.nextInt();
-
             System.out.print("Enter the number for the text inside the \"{...}\" to be subtracted: ");
             int num = scanner.nextInt();
             indexNumMap.put(new IndexData(startIndex, endIndex, num), num);
@@ -65,13 +59,11 @@ public class TesterClass {
             System.out.print("Do you want to use Caesar Cipher for further encryption - &num{}? [y/n]: ");
             useCC = scanner.next();
         }
-        //Applying Caesar Cipher - &num{}
         String AESDecryptedText = "";
         System.out.println("Current encrypted text with Caesar Cipher: " + encryptedText);
-        System.out.print("Do you want to use AES decryption? [y/n]: ");//3
+        System.out.print("Do you want to use AES decryption? [y/n]: ");
         String useDec = scanner.next();
-        if (useDec.equals("y")) {
-            //Removal of Caesar Cipher - &num{}
+        if (useDec.equals("y")) { //Removal of Caesar Cipher - &num{}
             for (Map.Entry<IndexData, Integer> entry : indexNumMap.entrySet()) {
                 IndexData indexData = entry.getKey();
                 Integer number = entry.getValue();
@@ -87,7 +79,7 @@ public class TesterClass {
 
         //Origin Text
         String oriText = "";
-        System.out.print("Do you want to obtain the origin text? [y/n]: ");//4
+        System.out.print("Do you want to obtain the origin text? [y/n]: ");
         String showOri = scanner.next();
         if (showOri.equals("y")) {
             decryptedIntermediateText = Decryption.SpecialSyntaxOperations(cipherText);
@@ -118,4 +110,3 @@ public class TesterClass {
         }
     }
 }
-//u+7dIvgWRI6ffc5yO3fO6N8fuO04l/HVfsUqcJjZ1Mjao6unonj1cw944xEckRVfGypcoxfTzNmzc1rbu8Y0m8kNCrywyu7um+7r0wAqCTvAUPzJnQtMUKLXhesFaCKIyq/CxNhP6xlfjwvKo8O9+w==
